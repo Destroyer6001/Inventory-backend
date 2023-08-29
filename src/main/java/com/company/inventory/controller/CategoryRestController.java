afa7bd3a2d5fce8af4abe7cd720eb2ddc668a875
Destroyer6001 <juanareiza6001@gmail.com>
2023-08-28 23:11:46 -0500

    Crud terminado

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -1,14 +1,22 @@
 package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CategoryRestController {
 	
 	@Autowired
@@ -18,6 +26,55 @@ public class CategoryRestController {
 	public ResponseEntity<CategoryResponseRest> searchCategory()
 	{
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
+	}
+	
+	/**
+	 * get categories by id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoryBiId(@PathVariable Long id)
+	{
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
+	}
+	
+	/**
+	 * post category
+	 * @param category
+	 * @return
+	 */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category)
+	{
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+	}
+	
+	/**
+	 * put category
+	 * @param id
+	 * @param category
+	 * @return
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> updateCategory(@PathVariable Long id, @RequestBody Category category)
+	{
+		ResponseEntity<CategoryResponseRest> response = service.update(id, category);
+		return response;
+	}
+	
+	/**
+	 * delete category
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> deleteCategory(@PathVariable Long id)
+	{
+		ResponseEntity<CategoryResponseRest> response = service.delete(id);
 		return response;
 	}
 }
--- src/main/java/com/company/inventory/services/CategoryServicesImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServicesImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,6 +39,158 @@ public class CategoryServicesImpl implements ICategoryService{
 		}
 		
 		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) 
+	{
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try
+		{
+			Optional<Category> category = categoryDao.findById(id);
+			
+			if(category.isPresent())
+			{
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Respuesta exitosa");
+			}
+			else
+			{
+				response.setMetadata("Respues Nok", "-1", "Categoria No Encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+			}
+		}
+		catch(Exception e)
+		{
+			response.setMetadata("Respues Nok", "-1", "Error al consultar por id");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try
+		{
+			Category categorySave = categoryDao.save(category);
+			
+			if(categorySave != null)
+			{
+				list.add(categorySave);
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "categoria guardada correctamente");
+			}
+			else
+			{
+				response.setMetadata("Respuesta Nok", "-1", "Debes haber ingresado mal un campo");
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.BAD_REQUEST);
+			}
+			
+		}
+		catch(Exception e)
+		{
+			response.setMetadata("Respues Nok", "-1", "Error guardar una categoria");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+		
+	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> update(Long id, Category category) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try
+		{
+			Optional<Category> categorySearch = categoryDao.findById(id);
+			
+			if(categorySearch.isPresent())
+			{
+				categorySearch.get().setName(category.getName());
+				categorySearch.get().setDiscription(category.getDiscription());
+				Category categoryUpdate = categoryDao.save(categorySearch.get());
+				
+				if(categoryUpdate != null)
+				{
+					list.add(categoryUpdate);
+					response.getCategoryResponse().setCategory(list);
+					response.setMetadata("Respuesta Ok", "00", "Categoria actualizada correctamente");
+				}
+				else
+				{
+					response.setMetadata("Respuesta Nok", "-1", "Error al actualizar la categoria" );
+					return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+				}
+			}
+			else
+			{
+				response.setMetadata("Respuesto Nok", "-1", "Categoria a actualizar no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+			}
+			
+		}
+		catch(Exception e)
+		{
+			response.setMetadata("Respues Nok", "-1", "Error al actualizar una categoria");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> delete(Long id) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try
+		{
+			Optional<Category> categorySearch = categoryDao.findById(id);
+			
+			if(categorySearch.isPresent())
+			{
+				categoryDao.deleteById(id);
+				list.add(categorySearch.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta Ok", "00", "Categoria eliminada");
+			}
+			else
+			{
+				response.setMetadata("Respuesta Nok", "-1", "Categoria a eliminar no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+			}
+			
+		}
+		catch(Exception e)
+		{
+			response.setMetadata("Respues Nok", "-1", "Error al eliminar una categoria");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+		
+		
 	}
 
 }
